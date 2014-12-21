0b46f945783732260473c0b405fa9572f107bb49
ppatel03 <ppatel03@g.syr.edu>
2014-12-21 03:41:55 -0500

    SOM C Sharp Code

---
+++ Program.cs
@@ -0,0 +1,269 @@
+﻿
+﻿/*Author: Ravi Nagendra
+ * 
+ * 
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.ComponentModel;
+
+namespace WordListGenerator
+{
+    using VectorData = Dictionary<int, List<int>>;
+
+    class WordList
+    {
+        public static string currentFilePath = "";
+        public static List<String> wordList = new List<String>();
+        public static String[,] matrixList;
+        public static List<String[,]> documentsMatrix = new List<String[,]>();
+        private static Excel.Workbook MyBook = null;
+        private static Excel.Application MyApp = null;
+        private static Excel.Worksheet MySheet = null;
+        public static String filename = "finalTaxonomy.xlsx";
+        public static VectorData finalVectorTable = new VectorData();
+        public static bool InitializeExcel()
+        {
+            bool excelSheetPresent = true;
+            MyApp = new Excel.Application();
+            MyApp.Visible = false;
+            currentFilePath = System.IO.Directory.GetCurrentDirectory() + "\\" + filename;
+            MyBook = MyApp.Workbooks.Open(currentFilePath);
+
+            if (MyBook == null)
+            {
+                return false;
+            }
+            MySheet = (Excel.Worksheet)MyBook.Sheets[1];
+            return excelSheetPresent;
+        }
+        public static void ReadExcel()
+        {
+            String appendFileContents = "";
+            bool uniqueString = true;
+            for (int index = 2; index <= 59; index++)
+            {
+                String currentString = (String)MySheet.get_Range("S" + index.ToString(), "S" + index.ToString()).Cells.Value;
+                appendFileContents = appendFileContents + currentString;
+            }
+
+            String[] currentWordsList = appendFileContents.Split(' ');
+
+            foreach (string st in currentWordsList)
+            {
+                for (int i = 0; i < wordList.Count; i++)
+                {
+                    if (wordList[i].CompareTo(st) == 0)
+                    {
+                        uniqueString = false;
+                        break;
+                    }
+                }
+                if (uniqueString)
+                {
+                    wordList.Add(st);
+                }
+                else
+                {
+                    //String is not unique so not adding
+                }
+                uniqueString = true;
+            }
+        }
+
+        public static String FindElementPosition(String[] currentWordsList, string rowElement, string columnElement)
+        {
+            int cooccurentCount = 0;
+            for (int i = 0; i < currentWordsList.Count(); i++)
+            {
+                if (i == 0)
+                {
+                    if (currentWordsList[i].CompareTo(rowElement) == 0)
+                    {
+                        if (currentWordsList[i + 1].CompareTo(columnElement) == 0)
+                        {
+                            cooccurentCount++;
+                        }
+                    }
+                }
+                else if (i == currentWordsList.Count() - 1)
+                {
+                    if (currentWordsList[i].CompareTo(rowElement) == 0)
+                    {
+                        if (currentWordsList[i - 1].CompareTo(columnElement) == 0)
+                        {
+                            cooccurentCount++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (currentWordsList[i].CompareTo(rowElement) == 0)
+                    {
+                        if (currentWordsList[i - 1].CompareTo(columnElement) == 0
+                            || currentWordsList[i + 1].CompareTo(columnElement) == 0)
+                        {
+                            cooccurentCount++;
+                        }
+                    }
+                }
+            }
+
+            return cooccurentCount.ToString();
+        }
+        public static void createMatrix()
+        {
+            for (int documentNumber = 2; documentNumber <= 59; documentNumber++)
+            {
+                String currentdocumentString = (String)MySheet.get_Range("S" + documentNumber.ToString(), "S" + documentNumber.ToString()).Cells.Value;
+                String[] currentWordsList = currentdocumentString.Split(' ');
+                matrixList = new string[wordList.Count(), wordList.Count()];
+                
+
+                for (int i = 0; i < wordList.Count; i++)
+                {
+                    for (int j = 0; j < wordList.Count; j++)
+                        matrixList[i, j] = "0";
+                }
+
+                for (int i = 0; i < wordList.Count; i++)
+                {
+                    String rowElement = wordList[i];
+
+                    var matchQuery = from word in currentWordsList
+                                     where word.ToString().CompareTo(rowElement) == 0
+                                     select word;
+
+                    int rowelementCount = matchQuery.Count();
+                    int rowIndex = 0;
+
+                    for (int j = 0; j < wordList.Count; j++)
+                    {
+                        String matrixRowColumnCount = "0";
+                        String columnElement = wordList[j];
+
+                        if (rowelementCount == 1 || rowelementCount == 0)
+                        {
+                            rowIndex = wordList.IndexOf(rowElement);
+                            break;
+                        }
+                        else
+                        {
+
+                            matrixRowColumnCount = FindElementPosition(currentWordsList, rowElement, columnElement);
+                        }
+                        matrixList[i, j] = matrixRowColumnCount;
+                    }
+
+                    if (rowelementCount == 1)
+                    {
+                        int previousIndex = 0;
+                        int nextIndex = 0;
+                        String previous = null;
+                        String next = null;
+
+                        if (rowIndex != 0)
+                        {
+                            previous = wordList[rowIndex - 1];
+                            previousIndex = wordList.IndexOf(previous);
+                            matrixList[i, previousIndex] = "1";
+
+                            if (rowIndex != wordList.Count - 1)
+                            {
+                                next = wordList[rowIndex + 1];
+                                nextIndex = wordList.IndexOf(next);
+                                matrixList[i, nextIndex] = "1";
+                            }
+                        }
+                        else if (rowIndex != wordList.Count - 1)
+                        {
+                            next = wordList[rowIndex + 1];
+                            nextIndex = wordList.IndexOf(next);
+                            matrixList[i, nextIndex] = "1";
+                        }
+                    }
+                }
+                documentsMatrix[documentNumber - 2] = matrixList;
+            }
+        }
+
+        public static void printMatrix()
+        {
+            for (int i = 0; i < wordList.Count(); i++)
+            {
+                for (int j = 0; j < wordList.Count; j++)
+                {
+                    Console.Write("{0}", matrixList[i, j]);
+                    Console.Write("\t");
+                }
+                Console.Write("\n");
+            }
+        }
+        public static void convertMatrixToVector()
+        {
+            int matrixSize = matrixList.GetLength(0);
+            for (int i = 0; i < 58; i++)
+            {
+                List<int> documentVectorContents = new List<int>();
+                finalVectorTable[i] = new List<int>();
+                String[,] currentMatrixList = documentsMatrix[i];
+
+                for (int row = 0; row < matrixSize; row++)
+                {
+                    for (int column = 0; column < matrixSize; column++)
+                    {
+                        String intermediate = currentMatrixList[row, column];
+                        documentVectorContents.Add(Convert.ToInt32(intermediate));
+                    }
+                }
+                finalVectorTable[i] = documentVectorContents;
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool initExcel = false;
+            initExcel = InitializeExcel();
+
+            if (!initExcel)
+            {
+                Console.WriteLine("Excel sheet not present");
+                return;
+            }
+            ReadExcel();
+
+            for (int i = 0; i < 58; i++)
+            {
+                documentsMatrix.Add(new String[,] { });
+            }
+            createMatrix();
+            convertMatrixToVector();
+            Console.WriteLine("Got the output");
+            Console.WriteLine("Co occurence matrix  is conveerted  to Vector");
+            Console.WriteLine("Dictionary size is " + wordList.Count);
+           /* foreach(KeyValuePair<int, List<int>> entry in finalVectorTable)
+            {
+            // do something with entry.Value or entry.Key
+                List<int> inputVector = entry.Value;
+                Console.Write("[");
+                foreach (int i in inputVector)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.Write("]");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            */
+            Console.WriteLine("Calling the  SOM Intialization ");
+            Console.WriteLine();
+            SOM.workOnSOMInitialization(finalVectorTable, wordList.Count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SOM.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace WordListGenerator
+{
+
+    class SOM
+    {
+        static Dictionary<int, List<int>> _randomValuesMap = new Dictionary<int, List<int>>();
+        static Dictionary<int, List<int>> _inputValuesMap = new Dictionary<int, List<int>>();
+
+        public static Dictionary<int, List<int>> inputValuesMap
+        {
+            get { return SOM._inputValuesMap; }
+            set { SOM._inputValuesMap = value; }
+        }
+        static int _maxRandom = 90000;
+        static int _totalNumber = 100;
+        static int _somMatrixSize = 10;
+        static int[,] _somMatrix = new int[10, 10];
+        static String[,] _documentMatrix = new String[10, 10];
+
+
+        public static String[,] documentMatrix
+        {
+            get { return SOM._documentMatrix; }
+            set { SOM._documentMatrix = value; }
+        }
+
+        public static int somMatrixSize
+        {
+            get { return SOM._somMatrixSize; }
+            set { SOM._somMatrixSize = value; }
+        }
+
+
+        public static int[,] somMatrix
+        {
+            get { return SOM._somMatrix; }
+            set { SOM._somMatrix = value; }
+        }
+
+
+        public static int totalNumber
+        {
+            get { return SOM._totalNumber; }
+            set { SOM._totalNumber = value; }
+        }
+
+        public static int maxRandom
+        {
+            get { return _maxRandom; }
+            set { _maxRandom = value; }
+        }
+
+        public static Dictionary<int, List<int>> randomValuesMap
+        {
+            get { return SOM._randomValuesMap; }
+            set { SOM._randomValuesMap = value; }
+        }
+        public static void generatedRandomVectorValues(Dictionary<int, List<int>> valueMap)
+        {
+            int i, j;
+            Random randomNumberGen = new Random();
+            for (i = 0; i < totalNumber; i++)
+            {
+                List<int> randomNumberList = new List<int>();
+                for (j = 0; j < maxRandom; j++)
+                {
+
+                    int randomNumber = randomNumberGen.Next(1, 10);
+                    randomNumberList.Add(randomNumber);
+
+                }
+
+                valueMap.Add(i, randomNumberList);
+            }
+
+        }
+
+        public static void createSOMMatrix()
+        {
+            //store the positions
+            int position = 0;
+            for (int i = 0; i < somMatrixSize; i++)
+            {
+                for (int j = 0; j < somMatrixSize; j++)
+                {
+                    somMatrix[i, j] = position++;
+                }
+            }
+        }
+
+        public static double computeEuclideanDistance(List<int> inputVector, List<int> SOMVector)
+        {
+            double squareDistance = 0;
+            double squareRootDistance = 0;
+            for (int i = 0; i < maxRandom; i++)
+            {
+                squareDistance += inputVector[i] * SOMVector[i];
+            }
+            squareRootDistance = Math.Sqrt(squareDistance);
+            return squareRootDistance;
+        }
+
+        public static void storeMinimumInDocumentMatrix(int documentNumber, double[,] distancesArray)
+        {
+            double minimum = distancesArray[0, 0];
+            int iPos = 0, jPos = 0;
+            for (int i = 0; i < somMatrixSize; i++)
+            {
+                for (int j = 0; j < somMatrixSize; j++)
+                {
+                    double distanceValue = distancesArray[i, j];
+                    if (distanceValue < minimum)
+                    {
+                        minimum = distanceValue;
+                        iPos = i;
+                        jPos = j;
+                    }
+
+                }
+            }
+            if (documentMatrix[iPos, jPos] == null)
+            {
+                documentMatrix[iPos, jPos] = documentNumber + "";
+            }
+            else
+            {
+                documentMatrix[iPos, jPos] += "," + documentNumber;
+            }
+
+            Console.WriteLine("Shortest distance is " + minimum + " found at documentMatrix[" + iPos + "," + jPos + "]");
+        }
+
+        public static void findTargetVector(int documentNumber, List<int> inputVector)
+        {
+            double[,] distancesArray = new double[10, 10];
+
+            for (int i = 0; i < somMatrixSize; i++)
+            {
+                for (int j = 0; j < somMatrixSize; j++)
+                {
+                    int somPosition = somMatrix[i, j];
+                    distancesArray[i, j] = computeEuclideanDistance(
+                        inputVector, randomValuesMap[somPosition]);
+                }
+            }
+
+            storeMinimumInDocumentMatrix(documentNumber, distancesArray);
+
+        }
+
+        public static void displayDocumentMatrix()
+        {
+            for (int i = 0; i < somMatrixSize; i++)
+            {
+                for (int j = 0; j < somMatrixSize; j++)
+                {
+                    Console.Write("[" + i + "," + j + "] = " + documentMatrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void workOnSOMInitialization(Dictionary<int, List<int>> inputVectorMap, int wordListCount)
+        {
+            maxRandom = wordListCount * wordListCount;
+            generatedRandomVectorValues(randomValuesMap);
+            createSOMMatrix();
+
+            foreach (KeyValuePair<int, List<int>> keyValuePair in inputVectorMap)
+            {
+                int documentNumber = keyValuePair.Key;
+                List<int> inputVector = keyValuePair.Value;
+                findTargetVector(documentNumber, inputVector);
+            }
+            displayDocumentMatrix();
+        }
+
+
+        /*
+        static void Main(string[] args)
+        {
+            generatedRandomVectorValues(inputValuesMap);
+            workOnSOMInitialization(inputValuesMap);
+            Console.ReadKey();
+            /*List<int> valueList = randomValuesMap[7];
+            foreach (int i in valueList)
+            {
+                Console.Write(i+"\t");
+            }
+           Console.ReadKey();
+        }*/
+    }
+}
